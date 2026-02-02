110fdf564a6e3cc7f32a2e49a672e0a0adcbf2d7
sebxs29 <sebas29t@gmail.com>
2026-02-02 12:11:56 -0500

    Update menuCliente.py
    Menú cliente final, solo falta agregar la parte de los otros integrantes

--- menuCliente.py
+++ menuCliente.py
@@ -1,6 +1,60 @@
 from cliente.arbolRegiones import construirArbolRegiones, mostrarArbol
 
+from cliente.seleccionCentros import (leerCentrosDisponibles, agregarCentro, eliminarCentro, mostrarCentros, ordenarCentros)
+
+from cliente.archivoRutas import guardarRutas
+
+# SUB MENU
+def menuSeleccionCentros(centrosSeleccionados, nombreCliente):
+
+    while True:
+        print("\n=== SELECCION DE CENTROS ===")
+        print("1. Agregar centro")
+        print("2. Eliminar centro")
+        print("3. Ver centros seleccionados")
+        print("4. Ordenar centros seleccionados")
+        print("5. Guardar y volver")
+        try:
+            opcion = int(input("Opcion: "))
+        except ValueError:
+            print("Error: Ingrese un numero\n")
+            continue
+
+        match opcion:
+            case 1:
+                centros = leerCentrosDisponibles()
+                for i, c in enumerate(centros, start=1):
+                    print(f"{i}. {c}")
+
+                try:
+                    op = int(input("Seleccione centro: "))
+                    centroElegido = centros[op - 1]
+                    agregarCentro(centrosSeleccionados, centroElegido)
+                except:
+                    print("Seleccion invalida")
+
+            case 2:
+                mostrarCentros(centrosSeleccionados)
+                if centrosSeleccionados:
+                    centro = input("Centro a eliminar: ")
+                    eliminarCentro(centrosSeleccionados, centro)
+
+            case 3:
+                mostrarCentros(centrosSeleccionados)
+
+            case 4:
+                ordenarCentros(centrosSeleccionados)
+
+            case 5:
+                guardarRutas(nombreCliente, centrosSeleccionados)
+                break
+            case _:
+                print("Opcion invalida\n")
+
+
 def menuCliente(nombreCliente):
+    centrosSeleccionados = []
+
     while True:
         print("\n=== MENU CLIENTE ===")
         print("1. Ver mapa de centros conectados")
@@ -27,7 +81,7 @@ def menuCliente(nombreCliente):
                 mostrarArbol(arbol)
 
             case 4:
-                print("\n=== CENTROS PARA ENVIO ===")
+                menuSeleccionCentros(centrosSeleccionados, nombreCliente)
                 
             case 5:
                 print("Saliendo del menu cliente...")
